7d902b65689ab8b8cfd7ad3b5ad2f757fafbe8f3
Lorde00 <68645684+Lorde00@users.noreply.github.com>
2023-12-23 05:55:05 -0300

    Add files via upload

---
+++ solicdados.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome!\n'))
+idade = str(input('Digite sua idade!\n'))
+numero = int(input('Digite seu telefone de contato!\n'))
+email = str(input('Digite seu email de contato!\n'))
+cidade = str(input('Digite a cidade onde você mora!\n'))
+
+print(f'Seu nome é {nome}, você tem {idade} anos e você mora em {cidade}!\n')
+print(f'Seu telefone de contato é {numero} e seu email é {email}!\n')
